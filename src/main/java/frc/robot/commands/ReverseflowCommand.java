8e7f033496a9072b81acde8e8fc9944f271629fc
Julian Steiner <juli.steiner@bluewin.ch>
2022-02-04 21:14:31 +0100

    #11 Ballsubsystem and commands

---
+++ src/main/java/frc/robot/commands/ReverseflowCommand.java
@@ -0,0 +1,42 @@
+package frc.robot.commands;
+
+import ch.fridolins.fridowpi.command.Command;
+import frc.robot.subsystems.ball.PickUp;
+import frc.robot.subsystems.ball.Transport;
+import frc.robot.subsystems.ball.base.PickUpBase;
+import frc.robot.subsystems.ball.base.TransportBase;
+
+public class ReverseflowCommand extends Command{
+    TransportBase transportModule;
+    PickUpBase pickupModule;
+
+    public ReverseflowCommand(TransportBase transportModule, PickUpBase pickUpModule) {
+        this.transportModule = transportModule;
+        this.pickupModule = pickUpModule;
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        addRequirements(transportModule, pickupModule);
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+        transportModule.reverse();
+        pickupModule.reverseBrush();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+        Transport.getInstance().stop();
+        PickUp.getInstance().stopBrush();
+    }
+}
---
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import ch.fridolins.fridowpi.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.ball.BallSubsystem;
+import frc.robot.subsystems.ball.base.TransportBase;
+
+public class ShootCommand extends SequentialCommandGroup {
+    private TransportBase transportSubmodule;
+
+    public ShootCommand(TransportBase transportSubmodule) {
+        this.transportSubmodule = transportSubmodule;
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        addCommands(new InstantCommand(() -> BallSubsystem.getInstance().setShooterToVelocity()), new TimerCommand(0.5),
+                new InstantCommand(() -> transportSubmodule.run()));
+        addRequirements(BallSubsystem.getInstance());
+    }
+}
