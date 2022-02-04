0ba49c07654c35ad11c5d668b22e2ddfab420334
Ambros <ambros.anrig@gmail.com>
2022-02-04 20:54:27 +0100

    Add initSendable

--- src/main/java/frc/robot/subsystems/climber/TelescopeArm.java
+++ src/main/java/frc/robot/subsystems/climber/TelescopeArm.java
@@ -162,4 +162,10 @@ public class TelescopeArm extends TelescopeArmBase {
         builder.addDoubleProperty("encoder pos", motors::getEncoderTicks, null);
         builder.addBooleanProperty("bottom limit switch", this::getBottomLimitSwitch, null);
     }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.addDoubleProperty("EncoderPosition", () -> motor.getEncoderTicks(), null);
+        super.initSendable(builder);
+    }
 }
