c992ed6e60cf0613ea66f2d83728a1823906808b
Gian-Laager <gianlaager114@gmail.com>
2022-02-18 22:03:35 +0100

    added ZeroTelescopeArm command

---
+++ src/main/java/frc/robot/commands/climber/ZeroTelescopeArm.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.climber;
+
+import ch.fridolins.fridowpi.command.Command;
+import frc.robot.subsystems.climber.TelescopeArm;
+
+public class ZeroTelescopeArm extends Command {
+    public ZeroTelescopeArm() {
+       requires(TelescopeArm.getInstance());
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        TelescopeArm.getInstance().startZero();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+        TelescopeArm.getInstance().resetEncoders();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return TelescopeArm.getInstance().getBottomLimitSwitch();
+    }
+}
